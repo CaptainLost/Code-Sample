db2e5ef32654f3ddf14e3746986f5c9c66a403ab
CaptainLost <biggun632@gmail.com>
2024-07-28 22:03:13 +0200

    Removed Duplicated Folder

--- Scripts/Agent Simulation/Agents/AgentSettings.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-using UnityEngine;
-
-[Serializable]
-public class AgentSettings
-{
-    [field: SerializeField]
-    public float StartHealth;
-}
--- Scripts/Agent Simulation/IDamagable.cs
+++
@@ -1,8 +0,0 @@
-public interface IDamagable
-{
-    float CurrentHealth { get; }
-    float MaxHealth { get; }
-
-    void ReceiveDamage(float amountOfDamage);
-    bool IsDead();
-}
